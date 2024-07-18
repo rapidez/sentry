3221dc7e279d3c74052cad7258ea7e76e0b792ab
Jade Geels <jade@justbetter.nl>
2024-07-18 15:07:38 +0200

    Remove error

--- resources/js/package.js
+++ resources/js/package.js
@@ -1,5 +1,3 @@
 if (import.meta.env.VITE_SENTRY_DSN !== undefined && window.config.sentry.enabled) {
     (() => import('./sentry.js'))()
 }
-
-window.setTimeout(() => { throw new Error('Bananen!') }, 5000)
