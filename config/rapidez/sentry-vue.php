9b8cad49674ecbebc28e770fee0dad5523f3b8a6
Jade Geels <jade@justbetter.nl>
2024-10-30 14:47:14 +0100

    Allow test errors to be sent from the browser console

--- config/rapidez/sentry-vue.php
+++ config/rapidez/sentry-vue.php
@@ -4,10 +4,12 @@ return [
     // Configuration as defined in the docs: https://docs.sentry.io/platforms/javascript/guides/vue/configuration/
     'configuration' => [
         'enabled' => env('SENTRY_VUE_ENABLED', true),
+        'allow_test_errors' => env('SENTRY_ALLOW_TEST_ERRORS', true),
 
         // Amount of errors to be logged to sentry (1.00 = 100%)
         'sampleRate' => env('SENTRY_VUE_SAMPLE_RATE', 100) / 100,
     ],
+
 
     // For integrations, see: https://docs.sentry.io/platforms/javascript/guides/vue/configuration/integrations/
     // If you want to add extra configuration to the constructor of an integration, change the `true` to an array like so:
