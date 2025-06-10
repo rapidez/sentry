a6aa125f9d197af0c848d7d508f6fff7e57aebec
Jade Geels <a@jgw.tf>
2025-06-10 09:33:59 +0200

    Add fallback for nonexistent stack trace

--- resources/js/filters.js
+++ resources/js/filters.js
@@ -3,6 +3,10 @@ import { addBeforeSendMethodHandler } from "./stores/useBeforeSendHandlers"
 if (window.config.sentry.filters.filterExternalUrls) {
     addBeforeSendMethodHandler((event) => {
         event.exception.values = event.exception.values.filter((error) => {
+            // Return true if we can't seem to check the stack trace
+            if (!error?.stacktrace?.frames?.[0]?.filename) {
+                return true
+            }
             return (new URL(error.stacktrace.frames[0].filename)).hostname == window.location.hostname
         })
 
